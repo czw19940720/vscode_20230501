b4d6c3158aec0322fb6bff1bdf6fcf5513fa56ed
czw19940720 <59766066+czw19940720@users.noreply.github.com>
2023-05-02 01:06:08 +0800

    Update test.cpp
    change this file

--- test.cpp
+++ test.cpp
@@ -39,20 +39,3 @@ public:
    }
 };
 
-int main(void)
-{
-   Rectangle Rect;
-   Triangle  Tri;
-
-   Rect.setWidth(5);
-   Rect.setHeight(7);
-   // 输出对象的面积
-   cout << "Total Rectangle area: " << Rect.getArea() << endl;
-
-   Tri.setWidth(5);
-   Tri.setHeight(7);
-   // 输出对象的面积
-   cout << "Total Triangle area: " << Tri.getArea() << endl;
-
-   return 0;
-}
